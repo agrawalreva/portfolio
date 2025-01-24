8a1e123c381a2892115d2c8761ab135229ad1f4c
Reva Agrawal <ragrawal@ucsd.edu>
2025-01-24 14:25:32 -0800

    dark mode

--- global.js
+++ global.js
@@ -4,6 +4,8 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
 let pages = [
   { url: '', title: 'Home' },
   { url: 'projects/', title: 'Projects' },
@@ -14,6 +16,8 @@ let pages = [
 
 let nav = document.createElement('nav');
 document.body.prepend(nav);
+
+url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
 
 for (let p of pages) {
   let url = p.url;
@@ -32,6 +36,38 @@ for (let p of pages) {
   nav.append(a);
 }
 
-const ARE_WE_HOME = document.documentElement.classList.contains('home');
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+  <label class="color-scheme">
+    Theme:
+    <select>
+      <option value="light dark">Automatic</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+    </select>
+  </label>
+  `
+);
 
-url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+const select = document.querySelector('.color-scheme select');
+
+// Function to set the color scheme and save it to localStorage
+function setColorScheme(colorScheme) {
+  document.documentElement.style.setProperty('color-scheme', colorScheme);
+  localStorage.colorScheme = colorScheme; // Save preference
+}
+
+// Listen for changes to the dropdown menu
+select.addEventListener('input', function (event) {
+  setColorScheme(event.target.value);
+});
+
+// Apply saved preference on page load
+if ('colorScheme' in localStorage) {
+  const savedScheme = localStorage.colorScheme;
+  setColorScheme(savedScheme);
+
+  // Update the dropdown to reflect the saved preference
+  select.value = savedScheme;
+}
