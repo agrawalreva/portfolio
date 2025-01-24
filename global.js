2f9b1660613561cae9bd1441039893be5bf22d15
Reva Agrawal <ragrawal@ucsd.edu>
2025-01-24 13:54:26 -0800

    dynamic nav bar

--- global.js
+++ global.js
@@ -4,10 +4,36 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-const navLinks = $$('nav a');
+let pages = [
+  { url: '/index.html', title: 'Home' },
+  { url: '/projects/index.html', title: 'Projects' },
+  { url: '/contact/index.html', title: 'Contact' },
+  { url: '/cv/index.html', title: 'CV' },
+  { url: 'https://github.com/agrawalreva', title: 'GitHub' },
+];
 
-const currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname
-);
+let nav = document.createElement('nav');
+document.body.prepend(nav);
 
-currentLink?.classList.add('current');
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+
+  url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add('current');
+  }
+
+  if (a.host !== location.host) {
+    a.target = '_blank';
+  }
+
+  nav.append(a);
+}
