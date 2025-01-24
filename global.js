8aede5f2983cf418aee8de60fb43fb270fde980f
Reva Agrawal <ragrawal@ucsd.edu>
2025-01-24 14:11:23 -0800

    updating nav

--- global.js
+++ global.js
@@ -12,21 +12,15 @@ let pages = [
   { url: 'https://github.com/agrawalreva', title: 'GitHub' },
 ];
 
-const ARE_WE_HOME = document.documentElement.classList.contains('home');
-
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 
 for (let p of pages) {
   let url = p.url;
   let title = p.title;
-
-  url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
-
   let a = document.createElement('a');
   a.href = url;
   a.textContent = title;
-
   a.classList.toggle(
     'current',
     a.host === location.host && a.pathname === location.pathname
@@ -35,6 +29,9 @@ for (let p of pages) {
   if (a.host !== location.host) {
     a.target = '_blank';
   }
-
   nav.append(a);
 }
+
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
+url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
