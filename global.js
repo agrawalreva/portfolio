6442629e7e4ad571c3da83255dd62e26774197d5
Reva Agrawal <ragrawal@ucsd.edu>
2025-01-24 17:21:17 -0800

    fixed nav bar and home img

--- global.js
+++ global.js
@@ -33,18 +33,3 @@ for (let p of pages) {
   }
   nav.append(a);
 }
-
-const form = document.querySelector('form');
-
-form?.addEventListener('submit', function (event) {
-  event.preventDefault();
-
-  const data = new FormData(form);
-
-  let url = form.action + '?';
-
-  for (let [name, value] of data) {
-    url = url + `${name}=${encodeURIComponent(value)}&`;
-  }
-  location.href = url;
-});
