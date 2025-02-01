9d6896cdf1adb8fb3146544789364b0406eb6be8
Reva Agrawal <ragrawal@ucsd.edu>
2025-01-31 19:51:04 -0800

    styling and project images

--- projects/projects.js
+++ projects/projects.js
@@ -1,10 +1,10 @@
 import { fetchJSON, renderProjects } from '../global.js';
 
-const projects = await fetchJSON('../lib/projects.json');
+const projects = await fetchJSON('../projects/projects.json');
 
 const projectsContainer = document.querySelector('.projects');
 
 const projectsTitle = document.querySelector('.projects-title');
 projectsTitle.textContent = `Projects (${projects.length})`;
-
+console.log("Fetched Projects:", projects);
 renderProjects(projects, projectsContainer, 'h2');
