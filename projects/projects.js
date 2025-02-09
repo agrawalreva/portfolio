b6ed3dee8f9c475e7059f14a8e831ac2c133ca04
Reva Agrawal <ragrawal@ucsd.edu>
2025-02-08 20:04:55 -0800

    creating pie chart

--- projects/projects.js
+++ projects/projects.js
@@ -1,10 +1,27 @@
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
 import { fetchJSON, renderProjects } from '../global.js';
 
 const projects = await fetchJSON('../projects/projects.json');
 
 const projectsContainer = document.querySelector('.projects');
-
 const projectsTitle = document.querySelector('.projects-title');
 projectsTitle.textContent = `Projects (${projects.length})`;
-console.log("Fetched Projects:", projects);
 renderProjects(projects, projectsContainer, 'h2');
+
+const svg = d3.select('#projects-pie-plot');
+
+const arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+let data = [1, 2, 3, 4, 5, 5];
+let sliceGenerator = d3.pie();
+let arcData = sliceGenerator(data);
+let arcs = arcData.map((d) => arcGenerator(d));
+
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
+
+arcs.forEach((arc, idx) => {
+    svg.append('path')
+        .attr('d', arc)
+        .attr('fill', colors(idx));
+});
+
